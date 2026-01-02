ce19ed223b9623fd332be77bed76bb3978aabdc5
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2026-01-02 13:02:30 +0100

    Small fixes and cleanup

--- src/admin/Enums/EntityHook.php
+++ src/admin/Enums/EntityHook.php
@@ -10,6 +10,7 @@ enum EntityHook: string implements HasLabel
 	case AFTER_TITLE = 'after-title';
 	case AFTER_SLUG = 'after-slug';
 	case AFTER_LAST = 'after-last';
+	case DEFAULT = 'default';
 
 	public function getLabel(): ?string
 	{
@@ -18,6 +19,7 @@ enum EntityHook: string implements HasLabel
 			self::AFTER_TITLE => 'After Title',
 			self::AFTER_SLUG => 'After Slug',
 			self::AFTER_LAST => 'After Last',
+			self::DEFAULT => 'Default',
 		};
 	}
 
