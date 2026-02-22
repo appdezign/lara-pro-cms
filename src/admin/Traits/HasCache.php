7abbb6931d470565a031cc68c773df306800ec89
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2026-02-22 18:18:23 +0100

    Cleanup Caching component

--- src/admin/Traits/HasCache.php
+++ src/admin/Traits/HasCache.php
@@ -2,22 +2,12 @@
 
 namespace Lara\Admin\Traits;
 
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\File;
-
 trait HasCache
 {
 
 	public static function clearCacheTypes(array $types = [], $force = true): bool
 	{
 		if ($force) {
-
-			/*
-			File::delete(base_path('bootstrap/cache/config.php'));
-			File::cleanDirectory(storage_path('framework/cache/data'));
-			File::cleanDirectory(storage_path('framework/views'));
-			File::cleanDirectory(storage_path('httpcache'));
-			*/
 
 			$types = [
 				'app_cache',
@@ -27,39 +17,19 @@ trait HasCache
 				'route_cache',
 			];
 
-			session()->push('laracacheclear', $types);
+			session(['laracacheclear' => $types]);
 
 			return true;
 
 		} else {
 
 			if (count($types) == 0) {
+
 				return false;
+
 			} else {
 
-				/*
-				if (in_array('app_cache', $types)) {
-					File::cleanDirectory(storage_path('framework/cache/data'));
-				}
-
-				if (in_array('config_cache', $types)) {
-					File::delete(base_path('bootstrap/cache/config.php'));
-				}
-
-				if (in_array('view_cache', $types)) {
-					File::cleanDirectory(storage_path('framework/views'));
-				}
-
-				if (in_array('http_cache', $types)) {
-					File::cleanDirectory(storage_path('httpcache'));
-				}
-
-				if (in_array('route_cache', $types)) {
-					// session(['laracacheclear' => true]);
-				}
-				*/
-
-				session()->push('laracacheclear', $types);
+				session(['laracacheclear' => $types]);
 
 				return true;
 
