4fc0808f86155ae10f553b0b028ae5d08154746f
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2026-09-14 11:12:47 +0200

    Add embedded media to SyncMedia

--- src/admin/Media/SyncMedia.php
+++ src/admin/Media/SyncMedia.php
@@ -3,6 +3,7 @@
 namespace Lara\Admin\Media;
 
 use Lara\Admin\Traits\HasMedia;
+use Lara\Common\Models\Entity;
 use Lara\Common\Models\ObjectImage;
 
 use Awcodes\Curator\Models\Media;
@@ -21,10 +22,42 @@ class SyncMedia
 			static::unlockMedia($item);
 		}
 
-		// sync
+		// sync attached media
 		$images = ObjectImage::all();
 		foreach ($images as $image) {
 			static::lockMedia($image->media);
+		}
+
+		// sync embedded media
+		$entities = Entity::whereIn('cgroup', ['page', 'block', 'entity'])->get();
+		foreach ($entities as $entity) {
+			$model = $entity->model_class;
+
+			$objects = $model::all();
+			foreach ($objects as $object) {
+				foreach ($media as $item) {
+					// check lead
+					if (str_contains($object->lead, $item->path)) {
+						static::lockMedia($item);
+					}
+
+					// check body
+					if (str_contains($object->body, $item->path)) {
+						static::lockMedia($item);
+					}
+
+					// check extra body fields, if any
+					$extraBodyFields = $entity->col_extra_body_fields;
+					if($extraBodyFields > 0) {
+						for($i = 2; $i <= $extraBodyFields + 1; $i++) {
+							if (str_contains($object->{"body{$i}"}, $item->path)) {
+								static::lockMedia($item);
+							}
+						}
+					}
+
+				}
+			}
 		}
 
 		return true;
